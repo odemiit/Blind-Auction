715fce9e868aeaad26b9ecae247e2c8688c8a623
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-06 19:32:16 +0000

    adjusting the input text to make the program more readable and to easily convert the bid to an integer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
-user_name = input("What is your name? ")
-user_bid = input("What is your bid? ")
+user_name = input("What is your name?: ")
+user_bid = input("What is your bid?: $")
+
+#convert the user bid from string to an integer
