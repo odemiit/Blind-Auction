2b5e8117546266fbe33489327ecf0d8e3a94a291
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-06 21:48:47 +0000

    call the add bit entry function inside the while loop and clear the screen if there's more players

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
+#empty bid log dictionary
+bid_log = {}
+
 def add_bid_entry(name, bid):
   #add the entry into an empty dictionary
-  bid_log = {}
-  bid_log[user_name] = user_bid
+  bid_log[name] = bid
 
 more_players_left = True
 
@@ -13,10 +15,15 @@ while more_players_left:
   user_name = input("What is your name?: ")
   user_bid = int(input("What is your bid?: $"))
 
+  #call on function to add bid entry
+  add_bid_entry(user_name, user_bid)
+
   #ask for more bidders
   more_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n")
 
   #switch more players left to False if answer is no so the while loop stops
   if more_bidders == "no":
     more_players_left = False
+  elif more_bidders == "yes":
+    clear()
 
