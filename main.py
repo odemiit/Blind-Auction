b9c02e6cb132dd4da830aa4463ffe4468de6fa94
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-06 20:04:03 +0000

    moved the user name an input into the while loop and created a function to add bid entries

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
-user_name = input("What is your name?: ")
-user_bid = input("What is your bid?: $")
-
-#convert the user bid from string to an integer
-user_bid = int(user_bid)
-
-#add the entry into an empty dictionary
-bid_log = {}
-bid_log[user_name] = user_bid
+def add_bid_entry(name, bid):
+  #add the entry into an empty dictionary
+  bid_log = {}
+  bid_log[user_name] = user_bid
 
 more_players_left = True
 
 while more_players_left:
-  #call on a function to ask for user name and bid
+  #ask for user name and bid
+  user_name = input("What is your name?: ")
+  user_bid = int(input("What is your bid?: $"))
 
   #ask for more bidders
   more_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n")
